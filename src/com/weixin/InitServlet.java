5631615e05326c9fbcbb6cda90fd7a4873c1d9c9
tallongsun <tallongsun@gmail.com>
2014-07-02 15:34:45 +0800

    服务器由tomcat6升级为jetty9.2.1，数据和缓存配置支持本地及正式发布两种情况

--- src/com/weixin/InitServlet.java
+++ src/com/weixin/InitServlet.java
@@ -5,6 +5,9 @@ import java.io.File;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.weixin.auth.TokenThread;
 import com.weixin.cache.CaoHelper;
 import com.weixin.db.DaoHelper;
@@ -12,6 +15,8 @@ import com.weixin.logic.chat.ChatService;
 
 public class InitServlet extends HttpServlet {
 	private static final long serialVersionUID = -6014103275870506517L;
+	
+	public static final Logger log = LoggerFactory.getLogger(RequestServlet.class);
 
 	public static String WEB_ROOT_PATH = "";
 	
@@ -21,7 +26,10 @@ public class InitServlet extends HttpServlet {
         TokenThread.init(getInitParameter("appid"), getInitParameter("appsecret"));
 		
 		WEB_ROOT_PATH = getServletContext().getRealPath("/");
-		String webInfPath = WEB_ROOT_PATH + "WEB-INF"+File.separator;
+		
+		log.debug("WEB_ROOT:"+WEB_ROOT_PATH);
+		
+		String webInfPath = WEB_ROOT_PATH + File.separator+"WEB-INF"+File.separator;
 		DaoHelper.init(webInfPath);
 		CaoHelper.init(webInfPath);
 		ChatService.init(webInfPath);
